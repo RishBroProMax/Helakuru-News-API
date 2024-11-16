71dad8f23fd4f02f74149b6493185eed72d38469
Rishmika Sandanu <104628935+RishBroProMax@users.noreply.github.com>
2024-11-16 19:54:20 +0530

    Helakuru API v0.5

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    API_KEY: 'your-api-key-here', // API Key for authentication
+    PORT: process.env.PORT || 3000, 
+    COPYRIGHT_TEXT: 'Powered by Helakuru Esana  - Built By ImRishmika!', 
+    NEWS_URL: 'https://www.helakuru.lk/esana/news/', 
+};
